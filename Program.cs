57b877512758b75ce26a9154bef36d809e76590c
NitroTech 4K <crtraer@gmail.com>
2022-10-12 14:05:07 -0300

    Started making HUD and ints for HUD

--- Program.cs
+++ Program.cs
@@ -8,8 +8,49 @@ namespace HealthSystemProject2022
 {
     internal class Program
     {
+        //HUD variables
+
+        static int health;
+
+        static int shield;
+
+        static int lives;
+
+        static int score;
+
+
+
+
+
         static void Main(string[] args)
         {
+            ShowHUD();
         }
+
+        
+
+
+
+        //Show the HUD
+
+        static void ShowHUD()
+        {
+            Console.WriteLine("========== Here are your stats ==========");
+            Console.WriteLine("=");
+            Console.WriteLine("=      Health: " + health + "            Shield: " + shield);
+            Console.WriteLine("=");
+            Console.WriteLine("=      Lives: " + lives + "              Score: " + score);
+            Console.WriteLine("=                                      ");
+
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+        
+
     }
 }
