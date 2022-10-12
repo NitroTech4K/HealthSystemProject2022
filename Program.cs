2f9d2bccae4e54a017e21f6b87e1bc73f05b277b
NitroTech 4K <crtraer@gmail.com>
2022-10-12 14:53:06 -0300

    Implimented new stats and API for IF statements

--- Program.cs
+++ Program.cs
@@ -16,10 +16,11 @@ namespace HealthSystemProject2022
 
         static int lives;
 
-        static int score;
+        static float score;
 
+        static float scoreMultiplier;
 
-
+        static string healthStatus;
 
 
         static void Main(string[] args)
@@ -35,18 +36,67 @@ namespace HealthSystemProject2022
 
         static void ShowHUD()
         {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("=========================================");
             Console.WriteLine("========== Here are your stats ==========");
-            Console.WriteLine("=");
+            Console.WriteLine("=========================================");
+            Console.WriteLine("=             " + healthStatus + "           ");
+            Console.WriteLine("=                                        ");
             Console.WriteLine("=      Health: " + health + "            Shield: " + shield);
-            Console.WriteLine("=");
+            Console.WriteLine("=                                       =");
             Console.WriteLine("=      Lives: " + lives + "              Score: " + score);
-            Console.WriteLine("=                                      ");
+            Console.WriteLine("=                                       =");
+            Console.WriteLine("=          Score Multiplier: " +  scoreMultiplier);
+            Console.WriteLine("=                                       =");
+            Console.WriteLine("=========================================");
 
 
             Console.ReadKey();
         }
 
 
+
+        //Health Status Check System
+
+
+        static void healthStatusCheck()
+        {
+            if (health == 100)
+            {
+                healthStatus = "Full Health";
+            }
+
+            else if (health >= 76 && health <=99)
+            {
+                healthStatus = "Good Health";
+            }
+
+            else if (health >= 51 && health <=75)
+            {
+                healthStatus = "Okay Health";
+            }
+
+            else if (health >= 26 && health <=50)
+            {
+                healthStatus = "Damaged";
+            }
+
+            else if (health >= 11 && health <= 25)
+            {
+                healthStatus = "=== !WARNING! ===";
+            }
+
+            else if (health >= 1 && health <= 10)
+            {
+                healthStatus = "=== !!!CRITICAL!!! ===";
+            }
+                
+            
+        }
+
+
+        
 
 
 
